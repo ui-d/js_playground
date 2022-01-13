fcc26b741f5d8c5e412deae3806b3451dc9bd8bb
ui-d <dawiddeveloper@gmail.com>
2022-01-13 22:37:21 +0100

    Factory function creational design pattern

---
+++ factory_&_constructor.js
@@ -0,0 +1,39 @@
+// factory function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function () {
+            console.log('draw');
+        }
+    };
+}
+
+function createRectangle(width, height) {
+    let rectangle = {}
+    rectangle.width = width;
+    rectangle.height = height;
+    rectangle.draw = function () {
+        console.log('draw');
+    }
+    return rectangle;
+}
+
+let circle = createCircle(1);
+let rectangle = createRectangle(1, 2);
+
+// separate method from factory function
+let behavior = {
+    draw: function () {
+        console.log('draw');
+    }
+}
+
+function createTriangle(height, color) {
+    let triangle = Object.create(behavior);
+    triangle.height = height;
+    triangle.color = color;
+    return triangle;
+}
+let triangle = createTriangle(3, "blue")
+
+triangle.hasOwnProperty('draw') // false
